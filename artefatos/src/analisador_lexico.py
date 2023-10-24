522119ca6c878925bee57ef82937eed91559767c
Renato Machado <renato.machado@sou.inteli.edu.br>
2023-10-24 17:30:18 -0300

    Code refactor

--- artefatos/src/analisador_lexico.py
+++ artefatos/src/analisador_lexico.py
@@ -9,42 +9,43 @@ class Token:
         return f"({self.tipo} {self.valor} {self.linha})"
 
 # test = "a = 1\nb = 12\nc = (12+3)"
-test = "programa :\ninicio\nprogramas = verdade\nverdades = 0\nse entao inicio\nses = verdades\nprogramas = ler()\nx = ler_varios(11, 4, 1)\nfim\nfim."
+test = "programa :\ninicio\nprogramas = verdade\nverdades = 0\nse entao inicio\nses = verdades\nprogramas = ler()\nx = ler_varios(11, 4, 1)\nfim\n\nfim."
 
 def variable_token(variableName, line):
     return Token("ID", variableName, line)
+
+def reserved_words_token(word, line):
+    comandoWords = ["ler", "ler_varios", "mostrar", "tocar", "mostrar_tocar", "esperar"]
+    booleanWords = ["verdade", "falso"]
+    sameWords = ["programa", "se", "entao", "senao", "enquanto", "faca", "nao"]
+
+    if word in comandoWords:
+        return Token("COMANDO", word, line)
+    elif word in booleanWords:
+        return Token("BOOLEAN", word, line)
+    elif word in sameWords:
+        return Token(word.upper(), word, line)
+    elif word == "inicio":
+        return Token("LBLOCK", word, line)
+    elif word == "fim":
+        return Token("RBLOCK", word, line)
+    elif word == "e":
+        return Token("OPMUL", word, line)
+    elif word == "ou":
+        return Token("OPSUM", word, line)
 
 def analisador_lexico(codigo):
     actualLine = 1
     token = []
     actualWord = ""
     possibilities = ["programa", "se", "entao", "senao", "enquanto", "faca", "nao", "inicio", "fim", "verdade", "falso", "ler", "ler_varios", "mostrar", "tocar", "mostrar_tocar", "esperar", "ou", "e"]
-    comandoWords = ["ler", "ler_varios", "mostrar", "tocar", "mostrar_tocar", "esperar"]
-    booleanWords = ["verdade", "falso"]
-    sameWords = ["programa", "se", "entao", "senao", "enquanto", "faca", "nao"]
 
     for i in range(0, len(codigo)):
         if codigo[i] != "\t":
             if codigo[i] == " " or codigo[i] == "\n" or i == len(codigo) - 1:
-                print(actualWord)
                 if len(actualWord) > 0:
                     if actualWord in possibilities:
-                        if actualWord in comandoWords:
-                            token.append(Token("COMANDO", actualWord, actualLine))
-                        elif actualWord in booleanWords:
-                            token.append(Token("BOOLEAN", actualWord, actualLine))
-                        elif actualWord in sameWords:
-                            token.append(Token(actualWord.upper(), actualWord, actualLine))
-                        elif actualWord == "inicio":
-                            token.append(Token("LBLOCK", actualWord, actualLine))
-                        elif actualWord == "fim":
-                            token.append(Token("RBLOCK", actualWord, actualLine))
-                        elif actualWord == "e":
-                            token.append(Token("OPMUL", actualWord, actualLine))
-                        elif actualWord == "ou":
-                            token.append(Token("OPSUM", actualWord, actualLine))
-                        
-                        print("chegou")
+                        token.append(reserved_words_token(actualWord, actualLine))
 
                         actualWord = ""
                     else:
