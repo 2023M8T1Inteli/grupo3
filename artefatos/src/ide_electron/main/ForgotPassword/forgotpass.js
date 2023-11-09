af416ef8f5dd89cd14d076dd666a8f2de670143f
Raphael <raphael.antunes@sou.inteli.edu.br>
2023-11-09 11:03:06 -0300

    esqueci a minha senha (primeira pagina

--- artefatos/src/ide_electron/main/ForgotPassword/forgotpass.js
+++ artefatos/src/ide_electron/main/ForgotPassword/forgotpass.js
@@ -1,12 +1,10 @@
-
-
-function validateEmail(email) {
+const validateEmail = (email) => {
     const emailRegex1 = /[A-Za-z0-9]+@[A-Za-z]+\.[A-Za-z]+/i;
     const emailRegex2 = /[A-Za-z0-9]+@[A-Za-z]+\.[A-Za-z]+\.[A-Za-z]/i;
     return emailRegex1.test(email) || emailRegex2.test(email);
 }
 
-function checkEmail(){
+const checkEmail = () => {
     var email = document.getElementById("email")
     var alert_email = document.getElementById("alert-email-format-error")
     var email_format_error = document.getElementById("wrong-format")
@@ -14,13 +12,13 @@ function checkEmail(){
     if(!validateEmail(email.value) && email_format_error.style.display == "" && email.value != ""){
         alert_email.style.display = "block"
         email_format_error.style.display = "block"
-    } else if(validateEmail(email.value)) {
+    } else if(validateEmail(email.value) || email.value == "") {
         alert_email.style.display = ""
         email_format_error.style.display = ""
     }
 }
 
-function checkInfo(){
+const checkInfo = () => {
     var email = document.getElementById("email")
     var alert_email = document.getElementById("alert-email-error")
     var email_error = document.getElementById("email-error")
@@ -34,53 +32,53 @@ function checkInfo(){
     }
 }
 
-const createCode = () => {
-    let randNum = 0;
-    let newCode = "";
-    for(let i = 0; i < 6; i++){
-      randNum = Math.floor(Math.random() * 10);
-      newCode += randNum;
-    }
+// const createCode = () => {
+//     let randNum = 0;
+//     let newCode = "";
+//     for(let i = 0; i < 6; i++){
+//       randNum = Math.floor(Math.random() * 10);
+//       newCode += randNum;
+//     }
     
-    newCode = parseInt(newCode);
-    return newCode;
-}
+//     newCode = parseInt(newCode);
+//     return newCode;
+// }
 
-const sendCode = (user) => {
-    const mailOptions = {
-      from: 'nimbbbus@gmail.com',
-      to: user.email,
-      subject: 'Código de Verificação',
-      text: user.message,
-    };
+// const sendCode = (user) => {
+//     const mailOptions = {
+//       from: 'nimbbbus@gmail.com',
+//       to: user.email,
+//       subject: 'Código de Verificação',
+//       text: user.message,
+//     };
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-          user: 'nimbbbus@gmail.com',
-          pass: 'ctov euiy fxjh raat',
-        },
-    });    
+//     const transporter = createTransport({
+//         host: 'smtp.gmail.com',
+//         port: 465,
+//         secure: true,
+//         auth: {
+//           user: 'nimbbbus@gmail.com',
+//           pass: 'ctov euiy fxjh raat',
+//         },
+//     });    
   
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error('Error sending email:', error);
-      } else {
-        console.log('Email sent:', info.response);
-      }
-    });
-};
+//     transporter.sendMail(mailOptions, (error, info) => {
+//       if (error) {
+//         console.error('Error sending email:', error);
+//       } else {
+//         console.log('Email sent:', info.response);
+//       }
+//     });
+// };
 
-function sendEmail(){
-    var email = document.getElementById("email")
-    var code = createCode()
+// const sendEmail = () => {
+//     var email = document.getElementById("email")
+//     var code = createCode()
 
-    const user = {
-        email: `${email.value}`,
-        message: "Seu código é: " + code
-    };
+//     const user = {
+//         email: `${email.value}`,
+//         message: "Seu código é: " + code
+//     };
     
-    sendCode(user);
-}
+//     sendCode(user);
+// }
