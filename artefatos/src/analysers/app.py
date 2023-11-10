b7568d306d72808fb1bbbb2c84438dd5e02b88a6
BartonThomas <99219127+BartonThomas@users.noreply.github.com>
2023-11-10 15:26:27 -0300

    feat: automatic call to python function implemented

--- artefatos/src/analysers/app.py
+++ artefatos/src/analysers/app.py
@@ -14,6 +14,6 @@ def read_file(file_name):
         raise Exception(f"File {path} not found")
     
 if __name__ == "__main__":
-    code = read_file("exemploS2.txt")
+    code = read_file("programa.txt")
     tokens = AnalisadorLexico(code).getTokens()
     print(AnalisadorSintatico(tokens).program())
--- artefatos/src/ide_electron/app.js
+++ artefatos/src/ide_electron/app.js
@@ -1,11 +1,12 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const fs = require('fs');
+const { spawn } = require('child_process'); // Import child_process to spawn Python
 
 let newPageWindow;
 let registerPage;
 
 app.on('ready', () => {
-  // Cria uma nova janela para a tela home.html
+  // Create a new window for the home.html screen
   newPageWindow = new BrowserWindow({
     width: 1280,
     height: 832,
@@ -13,52 +14,60 @@ app.on('ready', () => {
       nodeIntegration: true,
       contextIsolation: false,
     }
-  }  
-  );
+  });
 
-  // Carrega a tela home.html
+  // Load the home.html screen
   newPageWindow.loadFile('./main/Home/home.html');
 
-
+  // Load the lab.html screen (You might want to add this to a separate event)
   newPageWindow.loadFile('./main/Lab/lab.html');
+
   registerPage = new BrowserWindow({
     width: 1280,
     height: 832,
   });
 
-  // Carrega a tela register.html
-registerPage.loadFile('./main/Register/register.html')
+  // Load the register.html screen (You might want to add this to a separate event)
+  registerPage.loadFile('./main/Register/register.html');
 
   ipcMain.on('open-home-page', () => {
     newPageWindow.show();
   });
 
   ipcMain.on('mensagem-para-processo-renderizador', (event, mensagem) => {
-
-
+    // Save the message to a file
     fs.writeFile('../test_file/programa.txt', mensagem, function(err) {
       if (err) {
-        console.log(err)
-    }
+        console.log(err);
       }
-    )
+    });
 
     try {
+      const pythonProcess = spawn('python', ['../analysers/app.py', mensagem]);
 
-      event.reply('resposta-do-processo-renderizador', 'Sucesso!');
-
-      dialog.showMessageBox(newPageWindow, {
-        type: 'info',
-        title: 'Feedback',
-        message: 'Sucesso!',
-        buttons: ['OK'],
-        icon: 'None'
+      pythonProcess.stdout.on('data', (data) => {
+        console.log(`Python stdout: ${data}`);
       });
-      
+
+      pythonProcess.stderr.on('data', (data) => {
+        console.error(`Python stderr: ${data}`);
+      });
+
+      pythonProcess.on('close', (code) => {
+        console.log(`Python process exited with code ${code}`);
+        event.reply('resposta-do-processo-renderizador', 'Sucesso!');
+
+        dialog.showMessageBox(newPageWindow, {
+          type: 'info',
+          title: 'Feedback',
+          message: 'Sucesso!',
+          buttons: ['OK'],
+          icon: 'None'
+        });
+      });
 
     } catch (e) {
       event.reply('resposta-do-processo-renderizador', e);
     }
-
   });
 });
