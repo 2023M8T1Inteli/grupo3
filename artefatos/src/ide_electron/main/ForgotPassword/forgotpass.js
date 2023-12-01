611385c4185c6cc9df2bf48ff5cef59da576a4b7
Raphael <raphael.antunes@sou.inteli.edu.br>
2023-12-01 14:53:36 -0300

    integração login

--- artefatos/src/ide_electron/main/ForgotPassword/forgotpass.js
+++ artefatos/src/ide_electron/main/ForgotPassword/forgotpass.js
@@ -86,16 +86,13 @@ const sendCode = async (user) => {
       text: user.message,
     };
 
-    console.log(nodemailer)
-
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
         auth: {
           user: 'dolphin.aacd@gmail.com',
-        //   pass: 'xmnb dnci ipah djxa',
-          pass: 'sjkdahskahdksa',
+          pass: 'xmnb dnci ipah djxa',
         },
     });    
   
@@ -117,18 +114,20 @@ const sendCode = async (user) => {
 
 function positiveFeedback(div){
     let message = "<p>Email enviado com sucesso</p>"
-    let positiveSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/></svg>'
+    let positiveSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/></svg>'
 
-    div.innerHTML = positiveSvg + message
-    div.style.backgroundColor = "rgba(0, 255, 0, 0.5)"
+    div.innerHTML = message + positiveSvg
+    div.style.backgroundColor = "rgba(52, 170, 52, 0.695)"
+    div.style.border = "1px solid white"
 }
 
 function negativeFeedback(div){
-    let negativeSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/></svg>'
-    let message = '<p id="mensage">Não foi possível enviar o email</p>'
-
-    div.innerHTML = negativeSvg + message
-    div.style.backgroundColor = "rgba(255, 0, 0, 0.5)"
+    let message = '<p> Não foi possível enviar o email </p>'
+    let negativeSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/></svg>'
+    
+    div.innerHTML = message + negativeSvg
+    div.style.backgroundColor = "rgba(255, 0, 0, 0.622)"
+    div.style.border = "1px solid white"
 }
 
 /**
@@ -153,12 +152,17 @@ const sendEmail = async () => {
     if(email_bool){
         positiveFeedback(feedback)
         setTimeout(() => {
+            feedback.innerHTML = ""
+            feedback.style.backgroundColor = ""
+            feedback.style.border = ""
             window.location.href = '../Confirm Code/confirmcode.html'
         }, 3000)
     } else {
         negativeFeedback(feedback)
         setTimeout(() => {
-            feedback.innerHTML == ""
+            feedback.innerHTML = ""
+            feedback.style.backgroundColor = ""
+            feedback.style.border = ""
         }, 3000)
     }
 }
