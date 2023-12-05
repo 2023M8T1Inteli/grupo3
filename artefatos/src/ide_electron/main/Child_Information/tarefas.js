31f1c1dca0e483b057e2da26c15728159f8df3ea
dtonavitor <vitor.barros@sou.inteli.edu.br>
2023-12-05 15:34:42 -0300

    feat: add modal to create task

--- artefatos/src/ide_electron/main/Child_Information/tarefas.js
+++ artefatos/src/ide_electron/main/Child_Information/tarefas.js
@@ -8,10 +8,6 @@ async function getTasks() {
     ipcRenderer.send('read-all-task')
 }
 
-async function getTasks(){
-    ipcRenderer.send('read-all-task')
-}
-
 async function getResponse(){
     const promise = new Promise((resolve) => {
         ipcRenderer.once('response-readAll-task', (event, arg) => {
@@ -28,24 +24,27 @@ async function getResponse(){
 }
 
 
-function createTask(task) {
+function createTaskCard(task) {
     let newTask = document.createElement('div');
     newTask.classList.add('card');
     newTask.innerHTML = `
-    <div class="topo"></div>
-    <p class="titulo">${task.name}</p>
-    <button class="btn-card desempenho" onclick="redirectToGraphic(${task.id})">Desempenho</button>
-    <button class="btn-card excluir excluir-novo" onclick="removeTask(${task.id})">Excluir</button>
+    <div onclick="goToLab('${task.name}', ${task.id})">
+        <div class="topo"></div>
+        <p class="titulo">${task.name}</p>
+        <button class="btn-card desempenho" onclick="redirectToGraphic(${task.id})">Desempenho</button>
+        <button class="btn-card excluir excluir-novo" onclick="removeTask(${task.id})">Excluir</button>
+    </div>
     `;
-    cardsContainer.append(newTask);
+    cardsContainer.prepend(newTask);
 }
 
 async function showTasks() {
+    console.log("showTasks")
     await getTasks()
     let tasks = await getResponse()
     if (tasks != null) {
         tasks.forEach(task => {
-            createTask(task.dataValues)
+            createTaskCard(task.dataValues)
         });
     }
 }
@@ -78,6 +77,7 @@ async function removeTask(id) {
                 feedback.innerHTML = ""
                 feedback.style.backgroundColor = ""
                 feedback.style.border = ""
+                feedback.style.display = "none"
             }, 3000)
         }
     }
@@ -90,6 +90,17 @@ function negativeFeedback(div, text){
     div.innerHTML = message + negativeSvg
     div.style.backgroundColor = "rgba(255, 0, 0, 0.622)"
     div.style.border = "1px solid white"
+    div.style.display = "flex"
+}
+
+function positiveFeedback(div, text){
+    let message = "<p>" + text + "</p>"
+    let positiveSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/><path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/></svg>'
+
+    div.innerHTML = message + positiveSvg
+    div.style.backgroundColor = "rgba(52, 170, 52, 0.695)"
+    div.style.border = "1px solid white"
+    div.style.display = "flex"
 }
 
 // Remove the 'active' class from all buttons
@@ -115,8 +126,56 @@ function setActiveButton(buttonId) {
 // The following ID will need to be adjusted according to the button that corresponds to the current HTML page
 document.addEventListener('DOMContentLoaded', () => setActiveButton('#btn-tarefas'));
 
-function redirectToLab() {
-    localStorage.removeItem('taskTitle')
+var closeModalButton = document.querySelector('.close');
+
+closeModalButton.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+function createTask() {
+    modal = document.querySelector('.modal');
+    modal.style.display = 'block';
+}
+
+function sendTask() {
+    
+    let taskName = document.querySelector('#task-name').value;
+
+    if (taskName == "") {
+        alert("Digite um nome para a tarefa!")
+    }
+    else {
+        localStorage.setItem('taskTitle', JSON.stringify(taskName));
+        ipcRenderer.send('register-task', {
+            name: taskName,
+        })
+
+        let taskResponse
+        ipcRenderer.on('response-register-task', (event, arg) => {
+            taskResponse  = arg
+            if (taskResponse.message == "Erro ao cadastrar uma nova tarefa") {
+                alert("Não foi possível criar a tarefa")
+            }
+            else {
+                let feedback = document.querySelector('#feedback')
+                let modal = document.querySelector('.modal');
+                modal.style.display = 'none';
+                positiveFeedback(feedback, "Tarefa criada com sucesso!")
+                setTimeout(() => {
+                    feedback.innerHTML = ""
+                    feedback.style.backgroundColor = ""
+                    feedback.style.border = ""
+                    feedback.style.display = "none"
+                    window.location.reload()
+                }, 3000)
+            }
+        })
+    }
+}
+
+function goToLab(taskName, taskId) {
+    localStorage.setItem('taskTitle', JSON.stringify(taskName));
+    localStorage.setItem('taskId', JSON.stringify(taskId));
     window.location.href = '../Lab/lab.html';
 }
 
